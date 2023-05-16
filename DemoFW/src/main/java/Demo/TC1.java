a72fdc1c074d05ea3b088145735514e7c13a5934
Pratik <Pratik@192.168.1.11>
2023-05-16 11:56:32 +0530

    ENGG1 CREATED IT 1ST

---
+++ DemoFW/src/main/java/Demo/TC1.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class TC1 {
+	@Test
+	public void m1() {
+		System.out.println("Hello... Framework created");
+	}
+}
